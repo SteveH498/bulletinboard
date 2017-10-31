db84cef1e1ed3bab787fbe1b032800f2c261e60f
D061640 <D061640@MUCN34064666A.emea.global.corp.sap>
2017-10-31 18:20:24 +0100

    Add handler for Bulletin deletion

--- src/main/resources/static/js/src/BulletinList.js
+++ src/main/resources/static/js/src/BulletinList.js
@@ -3,20 +3,37 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { fetchBulletins } from './actions';
 
-import List, { ListItem, ListItemText } from 'material-ui/List';
+import List, { ListItem, ListItemText,  ListItemSecondaryAction } from 'material-ui/List';
 
 import Grid from 'material-ui/Grid';
+import DeleteIcon from 'material-ui-icons/Delete';
+import IconButton from 'material-ui/IconButton';
 
 class BulletinList extends React.Component {	
+
+    constructor(props) {
+	    super(props);  
+	    // This binding is necessary to make `this` work in the callback
+	    this.handleDelete = this.handleDelete.bind(this);
+	  }	
 	
 	componentDidMount() {
 		this.props.dispatch(fetchBulletins());		
 	}
 	
+	handleDelete(event){
+		console.log("Delet Bulletin");
+	}
+	
   render(){			  
 	 const bulletinsList = this.props.bulletins.map((bulletin) =>	 	
-	 	<ListItem default>
-	  		<ListItemText primary={bulletin.userID} secondary={bulletin.message} key={bulletin.id} />
+	 	<ListItem key={bulletin.id} default>
+	  		<ListItemText primary={bulletin.userID} secondary={bulletin.message}  />
+	  		<ListItemSecondaryAction>
+	            <IconButton aria-label="Delete" onClick={this.handleDelete}>
+	              <DeleteIcon />
+	            </IconButton>
+          </ListItemSecondaryAction>
 	  	</ListItem>		
 	  );
 	  return(
